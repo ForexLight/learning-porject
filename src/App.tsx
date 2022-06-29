1bc5c7ecc0f2627d3c9a3a805fca4709f08c42b9
Vlad Bugaets <vladbugaets@gmail.com>
2022-06-29 22:27:45 +0300

    scss and scss module support added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import styles from './app.module.scss'
+import stules2 from './app1.module.scss'
+
+
 export interface HelloWorldProps {
     userName: string;
     lang: string;
 }
 export const App = (props: HelloWorldProps) => (
-    <h1>
-        Hi {props.userName} from React! Welcome to {props.lang}!
+    <h1 className={styles.text}>
+        Hi {props.userName} from React! <span className={stules2.text2}>Welcome</span> to {props.lang}!
     </h1>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+let isDevelopment = true
+
 module.exports = {
     entry: "./src/index.tsx",
-    target: "web",
-    mode: "development",
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js",
     },
     resolve: {
-        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+        extensions: ['.js', '.ts', '.jsx', '.tsx']
     },
+    target: "web",
+    mode: "development",
     module: {
         rules: [
             {
@@ -19,18 +22,21 @@ module.exports = {
                 loader: "ts-loader",
             },
             {
-                enforce: "pre",
                 test: /\.js$/,
                 loader: "source-map-loader",
             },
             {
-                test: /\.css$/,
-                loader: "css-loader",
-            },
+                test: /\.(s(a|c)ss)$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({template: path.resolve(__dirname, "src", "index.html")}),
-        new MiniCssExtractPlugin({filename: "./src/yourfile.css",}),
+        new MiniCssExtractPlugin()
     ],
+
+    devServer: {
+        hot: true
+    },
 };
