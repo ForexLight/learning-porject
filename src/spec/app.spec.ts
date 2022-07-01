d2c4964539c23a29e3b35405f068c7311171d482
Vlad Bugaets <vladbugaets@gmail.com>
2022-07-01 15:24:54 +0300

    jest work with components fixed

--- src/spec/app.spec.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { act } from 'react-dom/test-utils'
-import * as ReactDOM from 'react-dom'
-import someFunc from './someFunc'
-
-it('should display ', () => {
-  expect(someFunc()).toBe(5)
-})
